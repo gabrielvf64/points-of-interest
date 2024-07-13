6d425ae8b0c6b9a5259dcecdc1a98c089066d576
gabriel.vicente <gabrielvf64@gmail.com>
2024-07-13 10:37:20 -0300

    feat: list all POI

--- src/main/java/com/gabrielvicente/point_of_interest/controller/PointOfInterestController.java
+++ src/main/java/com/gabrielvicente/point_of_interest/controller/PointOfInterestController.java
@@ -1,11 +1,12 @@
 package com.gabrielvicente.point_of_interest.controller;
 
+import com.gabrielvicente.point_of_interest.entity.PointOfInterest;
 import com.gabrielvicente.point_of_interest.request.CreatePointOfInterestRequest;
 import com.gabrielvicente.point_of_interest.service.PointOfInterestService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PointOfInterestController {
@@ -21,5 +22,12 @@ public class PointOfInterestController {
 
         pointOfInterestService.createPointOfInterest(request);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/points-of-interests")
+    public ResponseEntity<Page<PointOfInterest>> listPointOfInterest(@RequestParam(name = "page", defaultValue = "0") Integer page,
+                                                                     @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
+        Page<PointOfInterest> paginatedPointsOfInterest = pointOfInterestService.findAll(PageRequest.of(page, pageSize));
+        return ResponseEntity.ok(paginatedPointsOfInterest);
     }
 }
--- src/main/java/com/gabrielvicente/point_of_interest/service/PointOfInterestService.java
+++ src/main/java/com/gabrielvicente/point_of_interest/service/PointOfInterestService.java
@@ -3,6 +3,8 @@ package com.gabrielvicente.point_of_interest.service;
 import com.gabrielvicente.point_of_interest.entity.PointOfInterest;
 import com.gabrielvicente.point_of_interest.repository.PointOfInterestRepository;
 import com.gabrielvicente.point_of_interest.request.CreatePointOfInterestRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,5 +19,9 @@ public class PointOfInterestService {
     public void createPointOfInterest(CreatePointOfInterestRequest request) {
         PointOfInterest entity = request.toEntity();
         repository.save(entity);
+    }
+
+    public Page<PointOfInterest> findAll(PageRequest pageRequest) {
+        return repository.findAll(pageRequest);
     }
 }
