8cdda646d2fd2a4a1e33ea8c5a06acf4c574f64d
gabriel.vicente <gabrielvf64@gmail.com>
2024-07-13 10:11:33 -0300

    initial commit

---
+++ src/test/java/com/gabrielvicente/point_of_interest/PointOfInterestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielvicente.point_of_interest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointOfInterestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
